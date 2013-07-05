07a143014806322ba7476d98261de0e7f8b5da1d
Shinobu KAWANO <kawanoshinobu@gmail.com>
2013-07-06 06:04:39 +0900

    changed demo app to kitchen sink app.

--- demo/app.js
+++ demo/app.js
@@ -1,70 +1,74 @@
-/*
-    This file is generated and updated by Sencha Cmd. You can edit this file as
-    needed for your application, but these edits will have to be merged by
-    Sencha Cmd when it performs code generation tasks such as generating new
-    models, controllers or views and when running "sencha app upgrade".
-
-    Ideally changes to this file would be limited and most work would be done
-    in other places (such as Controllers). If Sencha Cmd cannot merge your
-    changes and its generated code, it will produce a "merge conflict" that you
-    will need to resolve manually.
-*/
-
-// DO NOT DELETE - this directive is required for Sencha Cmd packages to work.
-//@require @packageOverrides
-
 //<debug>
 Ext.Loader.setPath({
-    'Ext': 'touch/src'
+    'Ext': '../../src'
 });
 //</debug>
 
+/**
+ * Ext.application is the heart of your app. It sets the application name, can specify the icon and startup images to
+ * use when your app is added to the home screen, and sets up your application's dependencies - usually the models,
+ * views and controllers that your app uses.
+ */
 Ext.application({
-    name: 'Flat',
+    name: 'Kitchensink',
+    //<feature charts>
+    requires: ['Kitchensink.view.ColorPatterns'],
+    //</feature>
 
-    requires: [
-        'Ext.MessageBox'
-    ],
-
-    views: [
-        'Main'
-    ],
-
-    icon: {
-        '57': 'resources/icons/Icon.png',
-        '72': 'resources/icons/Icon~ipad.png',
-        '114': 'resources/icons/Icon@2x.png',
-        '144': 'resources/icons/Icon~ipad@2x.png'
-    },
-
-    isIconPrecomposed: true,
-
+    //sets up the icon and startup screens for when the app is added to a phone/tablet home screen
     startupImage: {
-        '320x460': 'resources/startup/320x460.jpg',
-        '640x920': 'resources/startup/640x920.png',
-        '768x1004': 'resources/startup/768x1004.png',
-        '748x1024': 'resources/startup/748x1024.png',
-        '1536x2008': 'resources/startup/1536x2008.png',
-        '1496x2048': 'resources/startup/1496x2048.png'
+        '320x460': 'resources/startup/Default.jpg', // Non-retina iPhone, iPod touch, and all Android devices
+        '640x920': 'resources/startup/640x920.png', // Retina iPhone and iPod touch
+        '640x1096': 'resources/startup/640x1096.png', // iPhone 5 and iPod touch (fifth generation)
+        '768x1004': 'resources/startup/768x1004.png', //  Non-retina iPad (first and second generation) in portrait orientation
+        '748x1024': 'resources/startup/748x1024.png', //  Non-retina iPad (first and second generation) in landscape orientation
+        '1536x2008': 'resources/startup/1536x2008.png', // : Retina iPad (third generation) in portrait orientation
+        '1496x2048': 'resources/startup/1496x2048.png' // : Retina iPad (third generation) in landscape orientation
     },
+
+    isIconPrecomposed: false,
+    icon: {
+        57: 'resources/icons/icon.png',
+        72: 'resources/icons/icon@72.png',
+        114: 'resources/icons/icon@2x.png',
+        144: 'resources/icons/icon@144.png'
+    },
+
+    //loads the views used by the app from the app/view folder
+    views: [
+        //component demos
+        'NestedList', 'BasicList', 'GroupedList', 'DisclosureList', 'SourceOverlay', 'Buttons',
+        'FormPanel', 'Sliders', 'ToolbarInput', 'Icons', 'BottomTabs',
+        'Map', 'Overlays', 'Tabs', 'Toolbars', 'InlineDataView', 'BasicDataView', 'HorizontalDataView',
+        'Video', 'Audio', 'Carousel', 'TouchEvents', 'ThemeSencha', 'ThemeAuto', 'ThemeBB', 'ThemeWindows',
+
+        //data and utility demos
+        'JSONP', 'YQL', 'Ajax', 'NestedLoading',
+
+        //card transition animation demos
+        'SlideLeft', 'SlideRight', 'SlideUp', 'SlideDown',
+        'CoverLeft', 'CoverRight', 'CoverUp', 'CoverDown',
+        'RevealLeft', 'RevealRight', 'RevealUp', 'RevealDown',
+        'Pop', 'Fade', 'Flip', 'Cube'
+
+        //<feature charts>
+        //charts/draw demos
+        ,'AreaChart', 'LineChart', 'ColumnChart', 'ColumnChartWithRenderer', 'ColumnChartStacked', 'ColumnChart3D',
+        'PieChart', 'FreeDraw', 'ScatterChart', 'PieChart3D', 'CandlestickChart', 'OHLCChart',
+        'LineChartWithMarker', 'LineChartWithRenderer', 'BarChart', 'RadarChart', 'PlotChart', 'GaugeChart', 'BubbleChart',
+        'VectorIcons'
+        //</feature>
+    ],
+
+    //loads app/store/Demos.js, which contains the tree data for our main navigation NestedList
+    stores: ['Demos', 'USD2EUR', 'OrderItems', 'StockPrice', 'List', 'Pie', 'Speakers'],
+
+    //the Kitchen Sink has Phone and Tablet modes, which rearrange the screen based on the type
+    //of device detected
+    profiles: ['Tablet', 'Phone'],
 
     launch: function() {
-        // Destroy the #appLoadingIndicator element
-        Ext.fly('appLoadingIndicator').destroy();
-
-        // Initialize the main view
-        Ext.Viewport.add(Ext.create('Flat.view.Main'));
-    },
-
-    onUpdated: function() {
-        Ext.Msg.confirm(
-            "Application Update",
-            "This application has just successfully been updated to the latest version. Reload now?",
-            function(buttonId) {
-                if (buttonId === 'yes') {
-                    window.location.reload();
-                }
-            }
-        );
+        Ext.create('Kitchensink.store.Speakers', { id: 'Speakers' });
+        Kitchensink.util.Proxy.process('feed.js');
     }
 });
